432ac5ad0f9e0ecc03f5550926c8262a1cfe7974
Ashley <tolenashley7@gmail.com>
2017-11-12 16:27:17 -0500

    Formatting, font-family, removed opacity: 0.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 $(document).foundation(
-    
+    $(window).scroll(function() {
+        $('.donor_powered_airbnb').each(function(i) {
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if (bottom_of_window > bottom_of_object) {
+                $(this).animate({'opacity':'1'}, 1500);
+            }
+        })
+    })
 );
